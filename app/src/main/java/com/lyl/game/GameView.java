33f6705060a6e2d3b23b754cc23cf6a669beac1b
林玉玲 <382158690@qq.com>
2019-08-13 15:23:21 +0800

    Initial commit

---
+++ app/src/main/java/com/lyl/game/GameView.java
@@ -0,0 +1,25 @@
+package com.lyl.game;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+/**
+ * create lyl on 2019-08-13
+ * </p>
+ */
+public class GameView extends View {
+    public GameView(Context context) {
+        super(context);
+    }
+
+    public GameView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public GameView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
